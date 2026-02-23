65847a7b58d48ee6b2069ecfe90e80972901897b
juanvazquez <jvazquez@abartyshealth.com>
2026-02-23 18:02:05 -0400

    More fixes to make package compatible with Django 5.0+

--- test_proj/urls.py
+++ test_proj/urls.py
@@ -13,16 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.urls import path, include
 
 urlpatterns = [
-    url(r"^api-auth/",
-        include("rest_framework.urls", namespace="rest_framework")),
-    url(r"^mfa/", include("deux.urls")),
-    url(r"^mfa/authtoken/",
-        include("deux.authtoken.urls", namespace="authtoken"),
-    ),
-    url(r"^mfa/oauth2/",
-        include("deux.oauth2.urls", namespace="oauth2"),
-    ),
+    path(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path(r"^mfa/", include("deux.urls")),
+    path(r"^mfa/authtoken/", include("deux.authtoken.urls", namespace="authtoken")),
+    path(r"^mfa/oauth2/", include("deux.oauth2.urls", namespace="oauth2")),
 ]
